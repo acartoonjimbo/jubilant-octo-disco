ac2934acdd8a4b8e67c62f1bb07a2e3fc96c2870
acartoonjimbo <jimleppert.s1@gmail.com>
2025-08-08 13:53:45 -0400

    Update vite.config.ts
    fixing for file structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: 'client',
   build: {
-    outDir: 'dist'
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173
   }
 })
