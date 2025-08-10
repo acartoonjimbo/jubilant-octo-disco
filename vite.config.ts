232b59de25bdd9ad845f6531b4ed717a535238d0
acartoonjimbo <jimleppert.s1@gmail.com>
2025-08-10 18:12:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env": env,
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist/client',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     },
-    plugins: [react()],
-  };
+  },
 });
